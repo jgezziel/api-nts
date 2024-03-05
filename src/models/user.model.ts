6b19237465dc3357604389bda4902ab49e528d08
jgezziel <gzl1910@gmail.com>
2024-03-05 13:31:37 -0600

    Update user.model.ts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,23 +1,27 @@
-import userSchema from '../schemas/User'
-import { ZodError } from 'zod'
+import User, { UserAttributes } from '../schemas/User'
 
 export const userModel = {
   getAllUsers: async () => {
-    return 'Get all users'
+    return await User.findAll({ attributes: { exclude: ['password'] } })
   },
   getUserById: async (id: number) => {
-    return `Get a user by id: ${id}`
+    const user = await User.findByPk(id, { attributes: { exclude: ['password'] } })
+    return user
   },
-  createUser: async (_input: {}) => {
-    try {
-      const user = userSchema.parse(_input)
-      return user
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return error.errors.map((err) => ({ message: err.message }))
-      }
+  createUser: async (input: UserAttributes) => {
+    const { email } = input
+    const emailExists = await User.findOne({ where: { email } })
+
+    if (emailExists != null) {
+      return new Error('Email already exists')
     }
-    return 'Not a valid user object'
+
+    try {
+      const newUser = await User.create(input)
+      return newUser
+    } catch (error) {
+      return error
+    }
   },
   updateUser: async (id: number, _input: {}) => {
     return `Update a user: ${id}`
