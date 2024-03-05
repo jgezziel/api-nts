336fa38d4d726b7dab2a05cd228154433fddc76a
jgezziel <gzl1910@gmail.com>
2024-03-05 13:29:21 -0600

    add sync sequelize

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,14 @@ const port: string = app.get('port')
 
 const init = async (): Promise<void> => {
   try {
-    await connection.authenticate()
+    await connection.sync({ force: false })
     console.log('Connection has been established successfully. ✔')
     app.listen(port, () => {
       console.log(`Server is listening on: http://localhost:${port}`)
     })
   } catch (error) {
     console.error('Error occurred:', error)
+    process.exit(1)
   }
 }
 /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
