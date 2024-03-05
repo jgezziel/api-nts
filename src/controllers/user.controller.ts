07449d123ac0745f6ef47df872ca7f09fd980b1f
jgezziel <gzl1910@gmail.com>
2024-03-05 13:31:18 -0600

    add controllers
    add read, readId, create

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,22 +1,67 @@
 import { Request, Response } from 'express'
 import { userModel } from '../models/user.model'
+import { validateUser } from '../schemas/User'
 
 export const userController = {
   getAllUsers: async (_req: Request, res: Response) => {
     const users = await userModel.getAllUsers()
-    return res.json(users)
+    if (users.length === 0) {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'Users not found'
+      })
+    }
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      users
+    })
   },
   getUserById: async (req: Request, res: Response) => {
     const { id } = req.params
     const user = await userModel.getUserById(parseInt(id, 10))
 
-    if (user !== '') return res.json(user)
-    return res.status(404).send('User not found')
+    if (user === null) {
+      return res.status(404).json({
+        status: 'error',
+        code: 404,
+        message: 'User not found'
+      })
+    }
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      user
+    })
   },
   createUser: async (req: Request, res: Response) => {
     const input = req.body
-    const newUser = await userModel.createUser(input)
-    return res.status(201).json(newUser)
+    const result = validateUser(input)
+    if (!result.success) {
+      return res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: JSON.parse(result.error.message)
+      })
+    }
+
+    const newUser = await userModel.createUser(result.data)
+
+    if (newUser instanceof Error) {
+      console.error(newUser.name)
+      return res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: newUser.message
+      })
+    }
+
+    return res.status(201).json({
+      status: 'success',
+      code: 201,
+      user: newUser
+    })
   },
   updateUser: async (req: Request, res: Response) => {
     const { id } = req.params
